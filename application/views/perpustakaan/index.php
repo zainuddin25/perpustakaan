d7f8e1c2c1d023397ade6b39642dfcb2ab5f969f
zainuddin25 <zanukjepara@gmail.com>
2022-03-30 12:34:40 +0700

    first commit

---
+++ application/views/perpustakaan/index.php
@@ -0,0 +1,114 @@
+<?php 
+
+if(isset($_POST["btn"]))
+{
+    var_dump($_POST);
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+</head>
+<body>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col text-end me-5 mt-4">
+                <a href="" class="me-3" style="text-decoration: none;" data-bs-toggle="modal" data-bs-target="#exampleModal"><i>Masuk</i></a>
+                <a href="" style="text-decoration: none;" data-bs-toggle="modal" data-bs-target="#exampleModal1"><i>Daftar</i></a>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="row">
+                    <div class="col-4">
+                        <div class="text-center" style="margin-top: 190px; margin-left: 20px;">
+                            <h1>Perpustakaan Kita</h1>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates officiis iusto</p>
+                            <div class="row">
+                                <div class="col-12 mx-auto">
+                                    <button class="btn btn-primary" style="margin-right: 20px;" data-bs-toggle="modal" data-bs-target="#exampleModal">Masuk</button>
+                                    <button class="btn btn-outline-primary" data-bs-toggle="modal" data-bs-target="#exampleModal1">Daftar</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-6">
+                        <img src="asset/6607.jpg" alt="" style="width: 700px; margin-top: 100px">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Masuk</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <form>
+                        <div class="mb-3">
+                            <label for="exampleInputEmail1" class="form-label">Nama</label>
+                            <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                        </div>
+                        <div class="mb-3">
+                            <label for="exampleInputPassword1" class="form-label">Kata Sandi</label>
+                            <input type="password" class="form-control" id="exampleInputPassword1">
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Batal</button>
+                        <button type="button" class="btn btn-primary">Masuk</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="modal fade" id="exampleModal1" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Masuk</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <form method="post">
+                        <div class="mb-3">
+                            <label for="exampleInputEmail1" class="form-label">Nama</label>
+                            <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                        </div>
+                        <div class="mb-3">
+                            <label for="exampleInputEmail1" class="form-label">Email</label>
+                            <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                        </div>
+                        <div class="mb-3">
+                            <label for="exampleInputPassword1" class="form-label">Password</label>
+                            <input type="password" class="form-control" id="exampleInputPassword1">
+                        </div>
+                        <input type="hidden" name="tanggal" value="<?php echo date('d - M - Y') ?>">
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Batal</button>
+                        <button type="button" class="btn btn-primary">Masuk</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ application/views/perpustakaan/sidebar.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+    <title>Perpustakaan Kita</title>
+  </head>
+  <body>
+    
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-2 bg-dark" style="height: 663px">
+                <div class="border-bottom mt-3 border-2 border-light text-light">
+                    <h3 class="text-center">User</h3>
+                    <p class="text-center">-- ---- ----</p>
+                </div>
+                <div class="border-bottom border-2 border-light mt-4">
+                    <a href="#" class="link-light" style="text-decoration: none;">
+                        <p style="font-size: 18px;" class="mt-2 ms-2"><i class="fa-solid fa-book"></i> &nbsp; Buku Terpinjam</p>
+                    </a>
+                    <a href="#" class="link-light" style="text-decoration: none;">
+                        <p style="font-size: 18px;" class="mt-2 ms-2 mb-4"><i class="fa-solid fa-rotate-left"></i> &nbsp; Pengembalian Buku</p>
+                    </a>
+                </div>
+                <div class="border-bottom border-2 border-light mt-4">
+                    <a href="#" class="link-light" style="text-decoration: none;">  
+                        <p class="mt-2 ms-2" style="font-size: 18px;"><i class="fa-solid fa-house-chimney"></i> &nbsp; Beranda</p>
+                    </a>
+                    <a href="#" class="link-light" style="text-decoration: none;">  
+                        <p class="mt-2 ms-2" style="font-size: 18px;"><i class="fa-solid fa-chart-line"></i> &nbsp; Statistik</p>
+                    </a>
+                    <a href="#" class="link-light" style="text-decoration: none;">  
+                        <p class="mt-2 ms-2 mb-4" style="font-size: 18px;"><i class="fa-solid fa-book-bookmark"></i> &nbsp; Daftar Buku</p>
+                    </a>
+                </div>
+                <div class="mt-4">
+                    <a href="#" class="link-light" style="text-decoration: none;">  
+                        <p class="mt-2 ms-2" style="font-size: 18px;"><i class="fa-solid fa-user"></i> &nbsp; Akun Anda</p>
+                    </a>
+                    <a href="#" class="link-light" style="text-decoration: none;">  
+                        <p class="mt-2 ms-2" style="font-size: 18px;"><i class="fa-solid fa-right-from-bracket"></i> &nbsp; Log Out</p>
+                    </a>
+                </div>
+            </div>
+
+        
